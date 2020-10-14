383c3e3ae4210a568c7ea2d05e92d952a84e16aa
Xouli <xouli@list.ru>
2020-10-14 16:03:40 +0700

    ШЛЯПА

---
+++ v3.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" enctype="multipart/form-data">
+        <laber>загружай файл:</laber><br/>
+        <input type="file" name="file"><br/>
+        <laber>отправить</laber><br/>
+        <input type="submit" name="ok"><br/>
+    </form>
+  
+<?php
+// $mysqli = new mysqli('localhost','root','','baza');
+// $mysqli-> query("SET NAMES 'utf8'");
+// $mysqli->close();
+$name = $_FILES['file']['name'];
+$name.=rand(0000,9999);
+$pth = $_FILES['file']['tmp_name'];
+$size = $_FILES['file']['size'];
+$type = $_FILES['file']['type'];
+if($size > 2* 1024 * 1024){
+    exit('превышают байты ') ;
+}
+else{
+    if(isset($_POST['ok'])){
+       
+             if(move_uploaded_file($pth, 'src/'.$name)){
+        echo 'файл загружен';
+        header('Location: index.html');
+        exit;
+                    }
+                    else 'не загружен';
+        
+       
+    }
+    
+}
+?>
+   
+</body>
+</html>
