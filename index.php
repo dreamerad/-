86a71fac202e789181010ee69ac5251755d6f9d5
dreamerad <dreameryandex.22013@gmail.com>
2020-10-14 15:04:50 +0600

    if расширений файлов v2

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <html>
 <h1>Загрузка файлов</h1>
-<?php
-if($info){
-    echo "$info";
-}
-?>
 <form method="POST"  name = 'upload_file' enctype="multipart/form-data">
 <input type="file" name = 'filename'>
 <input type = 'submit' value = 'Загрузить'>
@@ -20,7 +15,7 @@ $blacklist = [".php", ".phtml", ".bmp", ".html", ".html", ".xml", ".php3", ".php
 
 // 1кб = 1024 б && 1 мб = 1024кб // 2мб =  2 * 1024 * 1024
 
-//}else{
+
 foreach  ($blacklist as $item) { // Перебор запрещенных расширений
     if (preg_match("/$item\$/i", $name)){ // Есть ли элемент из массива в названии
         echo "Ошибка, загрузить можно только картинку"; return false;
