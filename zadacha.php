c9aa3be459b5ce4383b2d9918e3de11fc11e917d
dreamerad <dreameryandex.22013@gmail.com>
2020-10-22 19:54:05 +0600

    угадай число

--- zadacha.php
+++ zadacha.php
@@ -1,41 +1,78 @@
 <?php
-$string = 'Есть длинный текст и есть форма поиска по этому тексту. При вводе слова в форму
-поиска необходимо найти все упоминания этого слова в тексте и выделить (подсветить) цветом, 
-жирным или другим настраивающим способом. В случае, если указываются 2 слова, то каждое должно 
-искаться индивидуально, если словосочетание указывается в кавычках,то ищется как единое словосочетание.
-Помимо грубого поиска так же должно находить слова с разными окончаниями: пиво, пива, пивом.';
-$array = explode(" ", $string);
-$ser = $_POST['searche'];
-// echo "<pre>";
-// var_dump($array);
-// echo "</pre>";
+session_start();
+// сумма четных положительных чисел массива
+$array = [2,2,3,4,5,122,1,3,4,4,2,2,2,2];
+$sum = 0;
+foreach($array as $val){
+  if($val > 0 && $val % 2 == 0){
 
-if(isset($_POST['ok'])){
-echo $string .'</br>';
-    foreach ($array as $index => $name)
- {
-   if  ($ser == $name)
-     {
-      echo '</br> совпадение - ' .$name. '</br>';
-      }
-    
+
+  }
+
+  $sum += $val;
 }
-}
-$array = array(1,1,1,2,2,2,3,3);
-foreach ($array as $val)
-{
-  if ( !$pr[$val] )
-  {
-    $pr[$val] = $val;
-    echo $pr[$val]. ' <br /> ';
+echo $sum. "</br>";
+// Определить, какое число в массиве встречается чаще всего.
+$tmp = array_count_values($array); 
+$max = max($tmp);
+$result = '';
+foreach($tmp as $val => $key){
+  if($key == $max){
+    $result .= $val;
   }
 }
+echo "Число " .$result. " Повторяется " .$max. " раз</br>";
 
+
+
+// Угадай число
+
+
+if (isset($_POST["guess"])) { 
+
+
+
+  $number = $_POST["number"]; //В переменную $number записываем данные полученные из текстового поля с именем number 
+  
+  
+  
+  $hidden_number = $_POST["hidden_number"]; //В переменную $hidden_number записываем данные полученные из скрытого поля с именем $hidden_number 
+  
+  
+  
+  
+  
+  
+ 
+  if ($number == $_POST["hidden_number"]) { 
+  
+  //Если числа равны, то мы выиграли
+  
+  
+  $hidden_number = rand(0, 50);
+  
+   } 
+  
+  } else { 
+  
+  $number = 0; 
+  
+  $hidden_number = rand(0, 50); //Загадываем число 
+  
+  } 
+  
+  
+  echo '<form action="?" method="POST">
+  
+  <input type="text" value="'.$number.'" name="number" /> <br />
+  
+  <input type="submit" name="guess" value="Играть" /><br/>
+  
+  <input type="hidden" name="hidden_number" value="'.$hidden_number.'" />
+  
+  </form>';
+  
 ?>
 
-<form method="POST">
-<input type="text" name="searche">
-<input type="submit" name="ok">
 
 
-</form>
