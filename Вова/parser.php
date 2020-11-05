4a5001e57bd52c1bc0366c809c123d41c47de7d1
dreamerad <dreameryandex.22013@gmail.com>
2020-11-06 00:42:12 +0600

    парс

--- Вова/parser.php
+++ Вова/parser.php
@@ -1,11 +1,19 @@
 <?php
 include 'simple_html_dom.php';
 
-$html = file_get_html('http://bymas.ru');
-$link = $html->find('div.f',0)->find('a');
-// var_dump($link);
-foreach($link as $values => $key){
-    echo $key->href. "</br>";
+$html = file_get_html('https://www.gazeta.ru/');
+$one = $html->find('nav[class=header20-nav]'); // Парсим название разделов
+foreach($one as $val){
+    $a = $val->find('a'); // Ищем все ссылки
+    
+    $two = file_get_html('https://www.gazeta.ru/' .$a->href); // Парсим название статьей
+    $two2 = $two->find('span.[itemprop="headline"]'); // Ищем название статьей
+    echo $val->plaintext. '</br>';
+    echo $two2->plaintext. '</br>';
 
+    var_dump($two);
+// break;
 }
+
+
 ?>
